232826511a4acc88db0d484051e8ce8f8fde09ca
Lucas Ausberger <ausbergerl@hotmail.com>
2022-12-03 22:53:13 -0600

    created unity project w/ client-server code

---
+++ morp/Assets/Scripts/Client.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public static Client instance;
+    public static int dataBufferSize = 4096;
+    public string ip = "localhost";
+    public int port = 3390;
+    public int myId = 1;
+    public TCP tcp;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Client instance already exists. Destroying.");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        tcp = new TCP();
+    }
+
+    public void ConnectToServer()
+    {
+        tcp.Connect();
+    }
+
+    public class TCP
+    {
+        public TcpClient socket;
+        private NetworkStream stream;
+        private byte[] receiveBuffer;
+
+        public void Connect()
+        {
+            socket = new TcpClient
+            {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
+        }
+
+        private void ConnectCallback(IAsyncResult _result)
+        {
+            socket.EndConnect(_result);
+
+            if (!socket.Connected)
+            {
+                return;
+            }
+
+            stream = socket.GetStream();
+            stream.BeginRead(receiveBuffer, dataBufferSize, 0, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            try
+            {
+                int _byteLength = stream.EndRead(_result);
+                if (_byteLength < 1)
+                {
+                    // TODO: disconnect
+                    return;
+                }
+                
+                byte[] _data = new byte[_byteLength];
+                Array.Copy(receiveBuffer, _data, _byteLength);
+
+                // TODO: handle data
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+            catch (Exception _e)
+            {
+                Console.WriteLine($"Error receiving TCP data: {_e}");
+            }
+        }
+    }
+}
---
+++ morp/Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    public GameObject startMenu;
+    public TMP_InputField usernameField;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("UIManager instance already exists. Destroying.");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectToServer()
+    {
+        startMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.instance.ConnectToServer();
+    }
+}
